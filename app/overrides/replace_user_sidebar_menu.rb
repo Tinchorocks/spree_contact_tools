d53f1c46fb30d70361f0af2866f4a8acb0cb7ef3
Tinchin <martin.mbosco@web-experto.com.ar>
2019-11-26 10:54:38 -0300

    Change override

--- app/overrides/replace_user_sidebar_menu.rb
+++ app/overrides/replace_user_sidebar_menu.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
   name: 'replace_user_sidebar_menu',
-  replace: 'ul:nth-child(14)',
+  replace: 'nav ul.nav:nth-child(5)',
   partial: 'spree/admin/shared/user_and_contact_tools'
 )
