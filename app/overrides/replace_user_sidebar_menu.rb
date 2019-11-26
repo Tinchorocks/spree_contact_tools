5368dc1f0064aa5566e6c709349ebbe19515c345
Tinchin <martin.mbosco@web-experto.com.ar>
2019-11-26 14:02:00 -0300

    Multiple fixes

--- app/overrides/replace_user_sidebar_menu.rb
+++ app/overrides/replace_user_sidebar_menu.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
   name: 'replace_user_sidebar_menu',
-  replace: 'nav ul.nav:nth-child(5)',
+  replace: "erb[silent]:contains('if Spree.user_class')",
+  closing_selector: "erb[silent]:contains('end')",
   partial: 'spree/admin/shared/user_and_contact_tools'
 )
