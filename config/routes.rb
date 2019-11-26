b1fa2840471f6cc07a4be052e7d4ff7d1393802b
Tinchin <martin.mbosco@web-experto.com.ar>
2019-11-26 09:53:55 -0300

    Newsletters routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Spree::Core::Engine.add_routes do
   # Add your extension routes here
   namespace :admin do
     resources :contacts
+    resources :newsletters
   end	
-
+  
   resources :contacts
+  resources :newsletters
 
   get '/contact_us', to: 'contacts#new'
   get '/contact_success', to: 'contacts#success'
